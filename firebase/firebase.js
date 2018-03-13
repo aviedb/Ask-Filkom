74c2a211125c60cd708ffdfcc75ce2b8ee109759
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2018-03-13 16:54:32 +0700

    Add firebase

---
+++ firebase/firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyBXNz5z8ua6E-DRb18vNxrpSFqIaBgRvLA",
+  authDomain: "ask-filkom.firebaseapp.com",
+  databaseURL: "https://ask-filkom.firebaseio.com",
+  projectId: "ask-filkom",
+  storageBucket: "ask-filkom.appspot.com",
+  messagingSenderId: "25348640375"
+}
+
+if(!firebase.apps.length) {
+  firebase.initializeApp(config)
+}
+
+const auth = firebase.auth()
+
+export {
+  auth
+}
