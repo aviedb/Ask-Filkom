8ea2f93fa3e0f69790abfa726fbf54282e945daa
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2018-03-23 17:13:25 +0700

    Add pages/search.js

---
+++ pages/search.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react'
+import Head from 'next/head'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import Paper from 'material-ui/Paper'
+import CircularProgress from 'material-ui/CircularProgress'
+
+import { firebase, auth, db } from '../firebase'
+import Header from '../components/header'
+import Footer from '../components/footer'
+import Questions from '../components/questions'
+import SignedInSidebar from '../components/signedInSidebar'
+import NotSignedInSidebar from '../components/notSignedInSidebar'
+
+const muiTheme = getMuiTheme({ userAgent: false })
+
+export default class Index extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      authUser: null,
+      questions: [],
+      loading: true,
+      countSearch: 0
+    }
+  }
+
+  componentDidMount() {
+    firebase.auth.onAuthStateChanged(authUser => {
+      authUser
+        ? this.setState(() => ({authUser}))
+        : this.setState(() => ({authUser: null}))
+    })
+
+    db.doGetQuestions((snapshot) => this.showQuestions(snapshot.val()))
+  }
+
+  showQuestions(snapshot) {
+    const { searchKeyword } = this.props.url.query
+
+    const temp = Object.entries(snapshot)
+      .map((item) => Object.assign({}, {key: item[0]}, item[1])).reverse()
+
+    let questions = []
+    let countSearch = 0
+
+    for (var i=0; i<temp.length; i++) {
+      let cek
+
+      temp[i].tags.map((tag) => {
+        if (tag.toUpperCase() === searchKeyword.toUpperCase())
+          cek = true
+      })
+
+      if(cek) {
+        questions.push(temp[i])
+        countSearch++
+      }
+    }
+
+    this.setState({
+      questions,
+      loading: false,
+      countSearch
+    })
+  }
+
+  render() {
+    const { questions, authUser, countSearch } = this.state
+
+    return(
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div>
+          <Head>
+            <link rel="stylesheet" href="../static/css/styles.css" />
+            <link rel="shortcut icon" href="../static/img/simplified_logo_favicon.ico"/>
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+              rel="stylesheet"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+            <title>Search: "{this.props.url.query.searchKeyword}" - ask-filkom</title>
+          </Head>
+          <Header user={this.state.authUser}/>
+          <div style={{ display: "flex" }}>
+            <div className="home__sidebar" style={{padding: "30px", paddingTop: "100px"}}>
+              { authUser
+                ? <SignedInSidebar authUser={authUser}/>
+                : <NotSignedInSidebar />
+              }
+            </div>
+            <div className="home__middle">
+              { this.state.loading &&
+                <Paper style={{width: "100%", marginTop: "10px", textAlign: "center", padding: "10px"}}>
+                  <CircularProgress color="rgb(39, 94, 130)"/>
+                </Paper>
+              }
+              { !this.state.loading &&
+                <h3 style={{ padding: "15px" }}>
+                  {countSearch} Search Result for: {this.props.url.query.searchKeyword}
+                </h3>
+              }
+              <ul>
+                {
+                  questions.map(question => (
+                    <Questions
+                      id={question.key}
+                      { ...question }
+                    />
+                  ))
+                }
+              </ul>
+            </div>
+            <div className="home__sidebar" style={{padding: "30px", paddingTop: "100px"}}>
+              <Paper style={{width: "100%", height: "100%", marginTop: "10px", padding: "10px"}}>
+                Ads
+              </Paper>
+            </div>
+          </div>
+          <Footer />
+        </div>
+      </MuiThemeProvider>
+    )
+  }
+}
